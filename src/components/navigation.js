fbbbd8697416ac38312490e4824e595e20300127
Pranav Ramesh <laphatize@protonmail.com>
2024-09-20 22:27:33 -0400

    feaT: init

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,9 +3,9 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: '-', href: '#', current: false },
+  { name: '-', href: '#', current: false },
+  { name: '-', href: '#', current: false },
 ]
 
 function classNames(...classes) {
